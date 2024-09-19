b072c5e3800c2e54af25e8f138abe60e121c7cbb
Sathwikshetty16 <113185195+Sathwikshetty16@users.noreply.github.com>
2024-09-19 20:42:56 +0530

    update

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,8 @@
 
+
 export const loginFunc = async (data) => {
     try {
-        const response = await fetch('https://rentify-server-harshpx.vercel.app/api/users/login', {
+        const response = await fetch('https://66e888fb490b560ad9f083bd--velvety-chaja-b21459.netlify.app/api/users/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -22,7 +23,7 @@ export const loginFunc = async (data) => {
 
 export const signupFunc = async (data) => {
     try {
-        const response = await fetch('https://rentify-server-harshpx.vercel.app/api/users/signup', {
+        const response = await fetch('https://66e888fb490b560ad9f083bd--velvety-chaja-b21459.netlify.app/api/users/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -37,7 +38,7 @@ export const signupFunc = async (data) => {
 
 export const uploadFunc = async (formData,user) => {
     try {
-        const response = await fetch('https://rentify-server-harshpx.vercel.app/api/users/upload', {
+        const response = await fetch('https://66e888fb490b560ad9f083bd--velvety-chaja-b21459.netlify.app/api/users/upload', {
             method: 'PUT',
             headers: {
                 'Authorization': `Bearer ${user.token}`
